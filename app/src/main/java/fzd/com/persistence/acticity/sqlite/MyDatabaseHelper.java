5f79f883f068a3988554eda988a0b7dcc596f732
skyrunwith <582547045@qq.com>
2016-05-18 19:13:37 +0800

    complete all sqlite

--- app/src/main/java/fzd/com/persistence/acticity/sqlite/MyDatabaseHelper.java
+++ app/src/main/java/fzd/com/persistence/acticity/sqlite/MyDatabaseHelper.java
@@ -9,12 +9,16 @@ import android.widget.Toast;
  * Created by Administrator on 2016/5/17.
  */
 public class MyDatabaseHelper extends SQLiteOpenHelper{
-    private String CREATE_BOOK = "create table book ("
+    public static final String CREATE_BOOK = "create table Book ("
                                 +"id integer primary key autoincrement,"
                                 +"author text,"
                                 +"price real,"
-                                +"page integer,"
+                                +"pages integer,"
                                 +"name text)";
+    public static final String CREATE_CATEGORY = "create table Category ("
+                                +"id integer primary key autoincrement,"
+                                +"category_name text,"
+                                +"category_code)";
     private Context mContext;
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -22,17 +26,38 @@ public class MyDatabaseHelper extends SQLiteOpenHelper{
     }
 
     /**
-     * 创建数据库
+     * 创建数据库,只有第一次运行时自动调用
      * @param sqLiteDatabase
      */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * 更新数据库
+     * 1.删除原来的表
+     * 2.调用onCreate();
+     * @param sqLiteDatabase
+     * @param i
+     * @param i1
+     */
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+//        sqLiteDatabase.execSQL("drop table if exists Book");
+//        sqLiteDatabase.execSQL("drop table if exists Category");
+//        onCreate(sqLiteDatabase);
+        /**
+         * 升级数据库最佳写法
+         */
+        switch (i){
+            case 1:
+                sqLiteDatabase.execSQL(CREATE_CATEGORY);
+            case 2:
+                sqLiteDatabase.execSQL("alter table Book add column category_id integer");
+            default:
+        }
     }
 }
--- app/src/main/java/fzd/com/persistence/acticity/sqlite/SqliteActivity.java
+++ app/src/main/java/fzd/com/persistence/acticity/sqlite/SqliteActivity.java
@@ -1,7 +1,11 @@
 package fzd.com.persistence.acticity.sqlite;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,17 +25,147 @@ public class SqliteActivity extends BaseActivity{
 
     }
 
+    //创建或升级数据库
+    private void createOrUpgrade(){
+        myDatabaseHelper.getWritableDatabase();
+    }
+
     @Override
     protected void initViews() {
         setContentView(R.layout.acitivity_sqlite);
         Button create_database = (Button) findViewById(R.id.create_database);
-        myDatabaseHelper = new MyDatabaseHelper(this, "BookStrore.db", null, 1);
+        myDatabaseHelper = new MyDatabaseHelper(this, "BookStrore.db", null, 3);
         create_database.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                myDatabaseHelper.getWritableDatabase();
+                createOrUpgrade();
             }
         });
+
+        Button upgrage_database = (Button) findViewById(R.id.upgrage_database);
+        upgrage_database.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createOrUpgrade();
+            }
+        });
+
+        Button add_data = (Button) findViewById(R.id.add_data);
+        add_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              add();
+            }
+        });
+        Button update_data = (Button) findViewById(R.id.update_data);
+        update_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db =  myDatabaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                db.update("book",values,"name = ?", new String[]{"The Da Vinci Code"});
+            }
+        });
+        Button delete_data = (Button) findViewById(R.id.delete_data);
+        delete_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              delete();
+            }
+        });
+
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               query();
+            }
+            });
+
+        Button replace_data = (Button) findViewById(R.id.replace_data);
+        replace_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transcation();
+            }
+        });
+
+    }
+
+    private void transcation() {
+
+        SQLiteDatabase db = myDatabaseHelper.getWritableDatabase();
+        try{
+            db.beginTransaction();
+            db.delete("Book",null, null);
+//            if(true){
+//                throw new NullPointerException();
+//            }
+            ContentValues values = new ContentValues();
+            values.put("name", "Game of Thrones");
+            values.put("author", "George Martin");
+            values.put("pages", 720);
+            values.put("price", 20.85);
+            db.insert("Book", null, values);
+            db.setTransactionSuccessful();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    private void query() {
+        SQLiteDatabase db = myDatabaseHelper.getWritableDatabase();
+        //  查询Book 表中所有的数据
+        Cursor cursor = db.query("Book", null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                //  遍历Cursor 对象，取出数据并打印
+                String name = cursor.getString(cursor.
+                        getColumnIndex("name"));
+                String author = cursor.getString(cursor.
+                        getColumnIndex("author"));
+                int pages = cursor.getInt(cursor.getColumnIndex
+                        ("pages"));
+                double price = cursor.getDouble(cursor.
+                        getColumnIndex("price"));
+                Log.d("SqliteActivity", "book name is " + name);
+                Log.d("SqliteActivity", "book author is " + author);
+                Log.d("SqliteActivity", "book pages is " + pages);
+                Log.d("SqliteActivity", "book price is " + price);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+    }
+
+    private void delete() {
+        SQLiteDatabase db =  myDatabaseHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("price", 10.99);
+        db.delete("Book", "pages > ?", new String[] { "500" });
+    }
+
+    private void add() {
+        SQLiteDatabase db =  myDatabaseHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        //  开始组装第一条数据
+        values.put("name", "The Da Vinci Code");
+        values.put("author", "Dan Brown");
+        values.put("pages", 454);
+        values.put("price", 16.96);
+        /**
+         * 参数：表名，列默认值，ContentValue
+         */
+        db.insert("Book", null, values); //  插入第一条数据
+        values.clear();
+        //  开始组装第二条数据
+        values.put("name", "The Lost Symbol");
+        values.put("author", "Dan Brown");
+        values.put("pages", 510);
+        values.put("price", 19.95);
+        db.insert("Book", null, values); //插入第二条数据
     }
 
     @Override
@@ -42,5 +176,6 @@ public class SqliteActivity extends BaseActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
